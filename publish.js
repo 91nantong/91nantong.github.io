5756e36a673959eb83bb32687bc259002164e99f
91nantong <91nantong@gmail.com>
2025-11-10 09:07:05 +0700

    Update URL in publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'edqqkhob.cc',
+	'kvgibjs.com',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
