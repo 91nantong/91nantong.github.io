ebcd84d2d69860c5071bdd871742529a2316c22c
91nantong <91nantong@gmail.com>
2025-11-12 18:31:27 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'kvgibjs.com',
+	'vvvypik.cc',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
