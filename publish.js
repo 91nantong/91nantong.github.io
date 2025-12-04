0e6f67d805c146ebb66c5029affa38b794391547
91nantong <91nantong@gmail.com>
2025-12-04 20:17:17 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'soeqgjaf.cc',
+	'ahqrmsf.xyz',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
