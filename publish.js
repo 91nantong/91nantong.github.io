ed7c98ec93fff1e1be1345b2759341f063cd37da
91nantong <91nantong@gmail.com>
2026-01-16 15:04:00 +0700

    Update URL in publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'xjjttbob.xyz',
+	'bwrbsnr.com',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
