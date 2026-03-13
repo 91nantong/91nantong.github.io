2e8896f4098730032b4b42b069699f09a98f3152
91nantong <91nantong@gmail.com>
2026-03-13 09:22:21 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'zjqjpmjk.com',
+	'mbeysnhe.cc',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
