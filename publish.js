397d35c12f88350e314421c9ed8b935d05529cf6
91nantong <91nantong@gmail.com>
2026-05-31 10:50:12 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'gyiveczsz.com',
+	'vvhowwmck.cc',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
