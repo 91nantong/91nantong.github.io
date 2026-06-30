1237a8f5e01ea1dfc3232a949ccc6cc382d3d7e4
91nantong <91nantong@gmail.com>
2026-06-30 19:17:41 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'kpdyvlgb.cc',
+	'xvkbkpil.cc',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
