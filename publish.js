4566b38455a3208d32897a3f88df91299f85d3ef
91nantong <91nantong@gmail.com>
2026-09-02 21:21:32 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'wijyitmkw.com',
+	'eofhibwd.cc',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
