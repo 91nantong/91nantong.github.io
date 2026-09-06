49d60c8ba74e5950a025a7820afaca801d017f6b
91nantong <91nantong@gmail.com>
2026-09-06 15:29:50 +0700

    Replace URL in publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'dkhxegzs.cc',
+	'tffccvid.cc',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
