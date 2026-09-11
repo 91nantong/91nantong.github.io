69227a7598791c7e144893fa4134254bd19c14ab
91nantong <91nantong@gmail.com>
2026-09-11 20:57:26 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'sagktlev.cc',
+	'jayfjwwtc.cc',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
