732c86830bf7d2d6f2bfdf90ee250a148ee5c0ea
91nantong <91nantong@gmail.com>
2026-09-18 09:17:13 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'eyqggpam.cc',
+	'xxtmcxte.com',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
